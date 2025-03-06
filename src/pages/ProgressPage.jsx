b09b3bad4deb25abb5c03d65bd77c53aa5c211e5
Kareem moore <moorekareem877@gmail.com>
2025-03-06 05:36:54 -0500

    ok

--- src/pages/ProgressPage.jsx
+++ src/pages/ProgressPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSettings } from '../context/SettingsContext';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Paper, List, ListItem, ListItemText } from '@mui/material';
 import { LineChart } from '@mui/x-charts/LineChart';
 
 const ProgressPage = () => {
@@ -17,14 +17,33 @@ const ProgressPage = () => {
   }, [settings.sessions]);
 
   return (
-    <Box sx={{ p: 3 }}>
+    <Box sx={{ p: 3, maxWidth: 800, margin: '0 auto', mt: 8 }}>
       <Typography variant="h4" gutterBottom>
-        Progress
       </Typography>
       <Typography variant="body1" gutterBottom>
         Track your progress here.
       </Typography>
-      <Box sx={{ mt: 3 }}>
+      <Paper sx={{ p: 2, mt: 3 }}>
+        <Typography variant="h6" gutterBottom>
+          Current Streak
+        </Typography>
+        <Typography variant="body1">
+          You are currently on a {settings.streak}-session streak!
+        </Typography>
+      </Paper>
+      <Paper sx={{ p: 2, mt: 3 }}>
+        <Typography variant="h6" gutterBottom>
+          Rewards
+        </Typography>
+        <List>
+          {settings.rewards.map((reward, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={reward} />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+      <Paper sx={{ p: 2, mt: 3 }}>
         <Typography variant="h6" gutterBottom>
           Timer Sessions
         </Typography>
@@ -38,17 +57,7 @@ const ProgressPage = () => {
           width={500}
           height={300}
         />
-      </Box>
-      <Box sx={{ mt: 3 }}>
-        <Typography variant="h6" gutterBottom>
-          Rewards
-        </Typography>
-        <ul>
-          {settings.rewards.map((reward, index) => (
-            <li key={index}>{reward}</li>
-          ))}
-        </ul>
-      </Box>
+      </Paper>
     </Box>
   );
 };
